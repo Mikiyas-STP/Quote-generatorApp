9cede1646800ccbe040dcfc8eeb65373407f9541
mikiyas-stp <mikiyasgebremichaell@gmail.com>
2025-09-22 22:09:43 +0100

    added functionality to add a new quote : POST

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const host = process.env.HOST || "0.0.0.0";
 
 app.use(cors());
-app.use(express.json());   // <-- THIS LINE IS CRUCIAL
+app.use(express.json());
 
 const quotes = [
   { author: "Benjamin Franklin", quote: "Either write something worth reading or do something worth writing." },
@@ -20,24 +20,18 @@ app.get("/", (req, res) => {
 
 app.post("/quotes", (req, res) => {
   const { author, quote } = req.body;
-  if (author && quote) {
-    quotes.push({ author, quote });
-    res.json({ success: true, message:"Quote added sucessfully"});}
-  else{
-    res.status(400).json({ sucess: false, message: "Invalid input. Both quote and author are required" });
-    }   
+  if (author && quote && author.trim() !== "" && quote.trim() !== "") {
+    quotes.push({ author: author.trim(), quote: quote.trim() });
+    res.status(201).json({ success: true, message: "Quote added successfully" });
+  } else {
+    res.status(400).json({ success: false, message: "Invalid input. Both quote and author are required" });
+  }
 });
 
-app.listen(port,host, () => {
-  console.log(`Server running at http://${port}:${port}`);
+app.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}`);
 });
 
-
-
-// To add a mew quote (POST)
-// curl -X POST -H "Content-Type: application/json" \
-//   --data '{"author":"Ibrahim","quote":"Hello"}' \
-//   http://127.0.0.1:3000/
-
+//http://127.0.0.1:3000/
 // fetch random quote (GET)
 // curl http://127.0.0.1:3000/
